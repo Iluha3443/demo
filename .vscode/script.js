ddf816e9b86dd40d126bee1905c96f6dfb2def76
Iluha3443 <tkacenkoila01@gmail.com>
2023-04-05 18:06:42 +0300

    !@#

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,7 +1,16 @@
 
-new Swiper('.image-slider', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable:true,
-    },
-});
+function makeArray(firstArray, secondArray, maxLength) {
+    
+for ( let i = 0; i < maxLength; i +=1) {
+let words = firstArray.concat(secondArray);
+  if (words>maxLength){
+  let  total = words = maxLength;
+    return total;
+  }
+  return words;
+}
+
+
+   
+  }
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea']))
