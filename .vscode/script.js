51ad07e01df7445edcbaf9e5124b6996cd595f39
Iluha3443 <tkacenkoila01@gmail.com>
2023-05-19 20:50:01 +0300

    212

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,16 +1,35 @@
-
-function makeArray(firstArray, secondArray, maxLength) {
-    
-for ( let i = 0; i < maxLength; i +=1) {
-let words = firstArray.concat(secondArray);
-  if (words>maxLength){
-  let  total = words = maxLength;
-    return total;
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 32,
+  email: "johndoe@example.com",
+  address: {
+    city: "New York",
+    state: "NY",
+    country: "USA"
   }
-  return words;
+};
+
+
+function getUserInfo (user)  {
+
+ const { firstName, lastName, address : { city, country } } = user;
+
+
+return {firstName, lastName, city, country }
 }
 
+const userInfo = getUserInfo(user);
 
-   
-  }
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea']))
+console.log(userInfo)
+
+
+
+
+
+
+
+// console.log(getFullName(user1)) // 25
+
+// console.log(getFullName(user2))// ; // null
+
