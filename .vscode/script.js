990a98f5aa360ea04e09dff171aa250ec556ffa6
Iluha3443 <tkacenkoila01@gmail.com>
2023-05-23 17:11:22 +0300

    фаывп

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,15 +1,18 @@
-const parentEl = document.getElementById('36');
-const alloEl = document.getElementById('1');
-const comfyEl = document.getElementById('2');
-const eldosEl = document.getElementById('3');
+
+
+const parentEl = document.getElementById('storm');
+
+
 
 parentEl.addEventListener('click', textNewElements )
 
 function textNewElements(event) {
-  console.log(event.target)
-  const newproblem = event.target;
-  newproblem.remove()
+  if (event.target.nodeName === 'BUTTON') {
+    const btnEl = document.createElement('button');
+    btnEl.classList.add('btn-new-style');
+    btnEl.textContent = 'Новая кнопка';
+    parentEl.appendChild(btnEl);
   }
+};
 
-
-
+ 
